f9bfd4c6cee63bfcf3508abef568c560c67996c3
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-07-03 20:43:47 +0300

    Assignments

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide=(parameter1,parameter2)=>parameter1/parameter2;
+divide(2000,100)
+
+const square=(parameter3)=>parameter3** 2;
+const add=(parameter5,parameter6)=>parameter5+parameter6;
